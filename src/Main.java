d1b79317eaf71715f5a864dc773a12bbd55aff6d
viktor <victorchernyaew@gmail.com>
2018-07-12 00:07:26 +0400

    создание проекта(дз для 1 урока)

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @version 12.07.2018
+ * @author Viktor Chernyaev
+ * @Java1 homework Lesson-1
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
