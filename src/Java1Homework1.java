65746c33aed7344f6e6936fb51c82a16806eae26
viktor <victorchernyaew@gmail.com>
2018-07-12 00:57:38 +0400

    написал метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат

--- src/Java1Homework1.java
+++ src/Java1Homework1.java
@@ -15,7 +15,15 @@ class Java1Homework1 {
         double f = 15.72775;
         boolean g = true;
         char r = 'A';
-        
+
+    }
+
+    public static int task1(int a, int b, int c, int d){
+        a = 2;
+        b = 3;
+        c = 7;
+        d = 16;
+        return a * (b + (c / d));
     }
 
 }
