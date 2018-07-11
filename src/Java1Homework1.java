f46594fc1e38b1c03abe07fae903934d683fe7f8
viktor <victorchernyaew@gmail.com>
2018-07-12 00:45:24 +0400

    создание переменных всех пройденных типов данных и инициализирование их значения.

--- src/Java1Homework1.java
+++ src/Java1Homework1.java
@@ -7,6 +7,15 @@
 class Java1Homework1 {
 
     public static void main(String[] args) {
-
+        byte a = 10;
+        short b = 2404;
+        int c = 200000;
+        long d = 15000L;
+        float e = 120.0f;
+        double f = 15.72775;
+        boolean g = true;
+        char r = 'A';
+        
     }
+
 }
