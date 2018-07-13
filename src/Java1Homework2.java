36b5fa5c527f2fe4f93b4db0cb1f9f050dc77a23
viktor <victorchernyaew@gmail.com>
2018-07-13 23:14:21 +0400

    Выполнил задание 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;

--- src/Java1Homework2.java
+++ src/Java1Homework2.java
@@ -10,6 +10,8 @@ class Java1Homework2 {
         task1();
         System.out.println("Задание 2:");
         task2();
+        System.out.println("Задание 3:");
+        task3();
     }
 
     public static void task1() {
@@ -28,10 +30,25 @@ class Java1Homework2 {
 
     public static void task2() {
         int[] nums = new int[8];
-        for (int i = 0, j =0; i < nums.length; i++, j +=3) {
-            nums[i]=j;
-            System.out.println(nums[i] +"");
+        for (int i = 0, j = 0; i < nums.length; i++, j += 3) {
+            nums[i] = j;
+            System.out.println(nums[i] + "");
 
         }
+    }
+
+    public static void task3() {
+        int[] nums = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.print("До: ");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+            if (nums[i] < 6) nums[i] *= 2;
+
+        }
+        System.out.print("После:  ");
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+
     }
 }
