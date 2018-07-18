1ff0081e3b0494ae0ee593ee54fe673a9c176ad7
viktor <victorchernyaew@gmail.com>
2018-07-18 19:04:27 +0400

    создание дз + импорт рандома

---
+++ src/Java1Homework3.java
@@ -0,0 +1,17 @@
+/**
+ * @version 18.07.2018
+ * @author Viktor Chernyaev
+ * @Java1 homework Lesson-3
+ */
+
+import java.util.Random;
+
+class Java1Homework3 {
+    public static void main(String args[]) {
+        Random rand = new Random();
+        int x = rand.nextInt(10);
+    }
+
+
+
+}
