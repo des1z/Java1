8316a7f7e9f3dad272229a42568a97c5d6bb9aeb
viktor <victorchernyaew@gmail.com>
2018-07-27 08:14:07 +0400

    написал классы, геттер,сеттер

--- src/Java1Homework5.java
+++ src/Java1Homework5.java
@@ -3,18 +3,40 @@
  * @author Viktor Chernyaev
  * @Java1 homework Lesson-4
  */
-public class Java1Homework5 {
-    public class Worker{
-        String fullname;
-        String position;
-        String email;
-        String phonenumber;
-        int salary;
-        int age;
+class Java1Homework5 {
+       public static void main(String[] args) {
+           Worker[] worker = new Worker[5];
+           worker[0] = new Worker("Ivanov Ivan Ivanovich", "Byhgalter", "ivanovivan@gmail.com", "89864531211", "35000", "41");
+           worker[1] = new Worker("Petrov Petr Petrovich", "manager", "petrovich99@gmail.com", "89765357423", "23500", "37");
+           worker[2] = new Worker("Pypkin Vasilii Ivanovich", "top manage", "pypkinvi@gmail.com", "89264537765", "37000", "44");
+           worker[3] = new Worker("Michael Sergeevich Voevodin", "system admin", "ivanovivan@gmail.com", "89023070701", "30000", "28");
+           worker[4] = new Worker("Chernyaew Viktor Sergeevich", "finance director", "ivanovivan@gmail.com", "89456785432", "55000", "49");
+
+           for (Worker a : worker) if (a.getAge() > 40) a.getInformation();
     }
-    public static void main(String[] args) {
-        Worker[] worker1 = new Worker[5];
+}
+
+class Worker{
+        String fullname, position, email, phone;
+        int salary, age;
 
 
-    }
+        Worker(String fullname, String position, String email, String phone, int salary, int age) {
+        this.fullname = fullname;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+}
+
+        void getInformation(){
+            System.out.println("fullname: " + fullname + "position: " + position + "email: " + email + "salary: " + salary + "age: " + age);
+        }
+
+        int getAge(){
+            return age;
+        }
+
+
 }
