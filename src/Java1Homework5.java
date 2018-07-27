4a6620468e730272c77280f17372c648fb668926
viktor <victorchernyaew@gmail.com>
2018-07-27 05:58:39 +0400

    написал класс worker

--- src/Java1Homework5.java
+++ src/Java1Homework5.java
@@ -4,4 +4,17 @@
  * @Java1 homework Lesson-4
  */
 public class Java1Homework5 {
+    public class Worker{
+        String fullname;
+        String position;
+        String email;
+        String phonenumber;
+        int salary;
+        int age;
+    }
+    public static void main(String[] args) {
+        Worker[] worker1 = new Worker[5];
+
+
+    }
 }
