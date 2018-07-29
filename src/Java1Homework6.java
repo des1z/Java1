fe956aa8c29a5c29e692171fb099a16e38b36f91
viktor <victorchernyaew@gmail.com>
2018-07-30 03:20:05 +0400

    создание проекта + создание классов

---
+++ src/Java1Homework6.java
@@ -0,0 +1,32 @@
+/**
+ * @version 30.07.2018
+ * @author Viktor Chernyaev
+ * @Java1 homework Lesson-6
+ * @link https://github.com/des1z
+ */
+public class Java1Homework6 {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        Cat c = new Cat();
+        Dog d = new Dog();
+
+    }
+}
+
+public class Animal{
+int run;
+int swim;
+int jump;
+
+}
+
+public class Cat extends Animal{
+
+}
+
+public class Dog extends Animal{
+
+}
+
+
+
